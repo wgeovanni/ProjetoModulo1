36e43322f789e4b9d64474df2cad99d7fa74516e
wgeovanni <willyangeo@gmail.com>
2023-04-13 23:54:10 -0300

    feat: Estilização da navbar e adição de título

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,32 @@
 import { Link } from 'react-router-dom'
+import "./header.css";
 
 export const Header = () => {
     return (
         <header>
-            <img src="../../../src/assets/logo.png" className="logo" alt="Logo do sistema" width="200px" />
             <nav className="menu">
-                <Link to='/cadastrofarmacia'>Farmácias</Link>
-                <Link to='/cadastromedicamento'>Medicamentos</Link>
-                <Link to='/listamedicamento'>Lista de Medicamentos</Link>
-                <Link to='/listafarmacia'>Lista de Farmácias</Link>
+                <div className="logo">
+                    <img src="../../../src/assets/logo.png" className="logo" alt="Logo do sistema" width="200px" />
+                </div>
+                <div className="titulo">
+                    <h1>PharmaSystem</h1>
+                </div>
+                <div className="links">
+                    <div className="dropdown">
+                        <button className="dropdown-btn">Cadastro</button>
+                        <div className="dropdown-content">
+                            <Link to='/cadastrofarmacia'>Farmácias</Link>
+                            <Link to='/cadastromedicamento'>Medicamentos</Link>
+                        </div>
+                    </div>
+                    <div className="dropdown">
+                        <button className="dropdown-btn">Listas</button>
+                        <div className="dropdown-content">
+                            <Link to='/listafarmacia'>Lista de Farmácias</Link>
+                            <Link to='/listamedicamento'>Lista de Medicamentos</Link>
+                        </div>
+                    </div>
+                </div>
             </nav>
         </header >
     );
