89b0ce8a6e8783adee637e5296b78be22b27f1ab
wgeovanni <willyangeo@gmail.com>
2023-04-13 23:59:59 -0300

    feat: Adição de Outlet no arquivo App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { Header } from './components/Header/Header'
+import { Outlet } from 'react-router-dom'
 
 export function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ export function App() {
   return (
     <div className="App">
       <Header />
+      <Outlet />
     </div>
   )
 }
