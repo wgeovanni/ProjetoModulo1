915164a1a4c5005dd6ff21f2176028725fe1d84a
wgeovanni <willyangeo@gmail.com>
2023-04-16 20:48:47 -0300

    feat: implementação da busca de farmacia no JSONServer

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -6,6 +6,12 @@ export const DataProvider = ({ children }) => {
 
     const [farmacia, setFarmacia] = useState([]);
 
+    useEffect(() => {
+        fetch("http://localhost:8080/farmacia")
+            .then((res) => res.json())
+            .then((farmacia) => setFarmacia(farmacia));
+    }, []);
+
     const dadosContexto = { farmacia };
 
     return (
