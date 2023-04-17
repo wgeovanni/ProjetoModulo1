c4cd54779c108a2303cfe266a735f0a29792816e
wgeovanni <willyangeo@gmail.com>
2023-04-17 20:14:29 -0300

    feat: captura dos dados do formulário

--- src/components/Formularios/FormCadMedicamento/FormCadMedicamentos.jsx
+++ src/components/Formularios/FormCadMedicamento/FormCadMedicamentos.jsx
@@ -2,36 +2,47 @@ import { useState } from "react"
 
 export const FormCadMedicamentos = () => {
 
-    const [nomeMed, setNomeMed] = useState("");
-    const [nomeLab, setNomeLab] = useState("");
-    const [dosagem, setDosagem] = useState("");
-    const [descricao, setDescricao] = useState("");
-    const [preco, setPreco] = useState("");
-    const [tipo, setTipo] = useState("");
+    const [medicamento, setMedicamento] = useState({
+        nomeMed: "",
+        nomeLab: "",
+        dosagem: "",
+        descricao: "",
+        preco: "",
+        tipo: "",
+    });
+
+    const atualizaCampo = (campo, valor) => {
+        const novoDado = { ...medicamento, [campo]: valor };
+        setMedicamento(novoDado);
+    }
+    const cadastraMed = () => {
+        event.preventDefault();
+        console.log(medicamento);
+    }
 
     return (
 
 
-        <form>
+        <form onSubmit={cadastraMed}>
             <label htmlFor="nomeMed">Nome do medicamento</label>
             <input type="text" name="nomeMed" id="nomeMed" required
-                onChange={(event) => setNomeMed(event.target.value)} />
+                onChange={(event) => atualizaCampo("nomeMed", event.target.value)} />
             <label htmlFor="nomeLab">Nome do laboratório</label>
-            <input type="text" name="nomeMed" id="nomeMed" required
-                onChange={(event) => setNomeLab(event.target.value)} />
+            <input type="text" name="nomeLab" id="nomeLab" required
+                onChange={(event) => atualizaCampo("nomeLab", event.target.value)} />
             <label htmlFor="dosagem">Dosagem</label>
             <input type="text" name="dosagem" id="dosagem" required
-                onChange={(event) => setDosagem(event.target.value)} />
+                onChange={(event) => atualizaCampo("dosagem", event.target.value)} />
             <label htmlFor="descricao">Descrição</label>
             <input type="textarea" name="descricao" id="descricao"
-                onChange={(event) => setDescricao(event.target.value)} />
+                onChange={(event) => atualizaCampo("descricao", event.target.value)} />
             <label htmlFor="preco">Valor Unitário</label>
             <input type="text" name="preco" id="preco" required
-                onChange={(event) => setPreco(event.target.value)} />
+                onChange={(event) => atualizaCampo("preco", event.target.value)} />
             <label htmlFor="tipo">Tipo de Medicamento</label>
-            <select name="tipo" id="tipo" required onChange={(evento) => setTipo(evento.target.value)}>
-                <option value="medControlado">Medicamento Controlado</option>
-                <option value="medComum">Medicamento Comum</option>
+            <select name="tipo" id="tipo" required onChange={(evento) => atualizaCampo("tipo", evento.target.value)}>
+                <option value="Medicamento Controlado">Medicamento Controlado</option>
+                <option value="Medicamento Comum">Medicamento Comum</option>
             </select>
             <input type="submit" />
         </form>
--- src/pages/Cadastro/CadMedicamento.jsx
+++ src/pages/Cadastro/CadMedicamento.jsx
@@ -1,5 +1,7 @@
+import { FormCadMedicamentos } from "../../components/Formularios/FormCadMedicamento/FormCadMedicamentos"
+
 export const CadMedicamento = () => {
     return (
-        <h1>Cadastro Medicamento</h1>
+        <FormCadMedicamentos />
     )
 }
