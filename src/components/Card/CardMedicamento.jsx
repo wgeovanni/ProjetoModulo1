e13d28e4e5bbcc681c856078a3b4257c86c6b62e
wgeovanni <willyangeo@gmail.com>
2023-04-18 19:13:30 -0300

    build: adição de bootstrap e instalação de bootstrap como dependência

--- src/components/Card/CardMedicamento.jsx
+++ src/components/Card/CardMedicamento.jsx
@@ -1,8 +1,7 @@
 export const CardMedicamento = ({ medicamento }) => {
 
     return (
-        <div>
-            <p>testecard</p>
+        <div className="container">
             <img className="remedio" src="../../../src/assets/medicamento.png" alt="Caixa de remédio"
                 title="Caixa de remédio" />
             <h3>{medicamento.nomeMed} {medicamento.dosagem}</h3>
