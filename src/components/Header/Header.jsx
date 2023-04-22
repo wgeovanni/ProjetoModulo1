d230dcd1c6ea7d784b3f7a457f9d80a55f60b7f0
wgeovanni <willyangeo@gmail.com>
2023-04-22 10:55:03 -0300

    style: alteração no estilo do cabeçalho

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,56 +6,65 @@ export const Header = () => {
     const { varHidden } = useData();
     return (
         <header>
-            <nav>
-                <ul className="nav nav-tabs d-flex justify-content-between">
-                    <li className="nav-item">
-                        <Link to={'/'} className="navbar-brand">
-                            <img src="../../../src/assets/logo.png" alt="Logo do sistema" width="50"
-                                className="d-inline-block align-text-top" />
-                            LABPharmacy Inc.
-                        </Link>
-                    </li>
-                    <div className="d-flex justify-content-between">
-                        <li className="nav-item dropdown-center">
-                            <button className="btn btn-outline-info me-2 nav-button dropdown-toggle"
-                                type="button" data-bs-toggle="dropdown" hidden={varHidden}>Cadastro</button>
-                            <ul className="dropdown-menu">
-                                <li>
-                                    <Link to='/cadastrofarmacia' className="dropdown-item">Farmácias</Link>
-                                </li>
-                                <hr className="dropdown-divider" />
-                                <li>
-                                    <Link to='/cadastromedicamento' className="dropdown-item">Medicamentos</Link>
-                                </li>
-                            </ul>
-                        </li>
-                        <li className="nav-item dropdown-center">
-                            <button className="btn btn-outline-info me-2 nav-button dropdown-toggle"
-                                type="button" data-bs-toggle="dropdown" hidden={varHidden}>Listas</button>
-                            <ul className="dropdown-menu">
-                                <li>
-                                    <Link to='/listafarmacia' className="dropdown-item">Lista de Farmácias</Link>
-                                </li>
-                                <hr className="dropdown-divider" />
-                                <li>
-                                    <Link to='/listamedicamento' className="dropdown-item">Lista de Medicamentos</Link>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <Link to='/' className="nav-item m-2">
-                                <button className="btn btn-outline-primary" hidden={!varHidden}
-                                >Login</button>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to='/' className="nav-item">
-                                <button className="btn btn-outline-primary" hidden={varHidden}>Logout</button>
-                            </Link>
-                        </li>
+            <div className="container" id="nav-container">
+                <nav className="navbar navbar-expand-lg">
+
+                    <Link to={'/'} className="navbar-brand">
+                        <img src="../../../src/assets/logo3.png" alt="Logo do sistema" />
+                    </Link>
+
+                    <button className="navbar-toggler mb-2" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbar-links" aria-controls="navbar-links" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse justify-content-end" id="navbar-links">
+                        <div className="navbar-nav">
+                            <div className="dropdown-center">
+                                <button className="btn btn-outline-info me-2 mb-2 dropdown-toggle"
+                                    type="button" data-bs-toggle="dropdown" hidden={varHidden}>Cadastro</button>
+                                <ul className="dropdown-menu">
+                                    <li>
+                                        <Link to='/cadastrofarmacia' className="dropdown-item">Farmácias</Link>
+                                    </li>
+                                    <hr className="dropdown-divider" />
+                                    <li>
+                                        <Link to='/cadastromedicamento' className="dropdown-item">Medicamentos</Link>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div className="dropdown-center">
+                                <button className="btn btn-outline-info me-2 mb-2 nav-button dropdown-toggle"
+                                    type="button" data-bs-toggle="dropdown" hidden={varHidden}>Listas</button>
+                                <ul className="dropdown-menu">
+                                    <li>
+                                        <Link to='/listafarmacia' className="dropdown-item">Lista de Farmácias</Link>
+                                    </li>
+                                    <hr className="dropdown-divider" />
+                                    <li>
+                                        <Link to='/listamedicamento' className="dropdown-item">Lista de Medicamentos</Link>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div className="nav-item dropdown-center">
+                                <Link to='/' className="nav-item mb-2">
+                                    <button className="btn btn-outline-primary" hidden={!varHidden}
+                                    >Login</button>
+                                </Link>
+                            </div>
+
+                            <div className="nav-item dropdown-center">
+                                <Link to='/' className="nav-item mb-2">
+                                    <button className="btn btn-outline-primary" hidden={varHidden}>Logout</button>
+                                </Link>
+                            </div>
+                        </div>
                     </div>
-                </ul>
-            </nav >
+                </nav >
+            </div>
         </header >
     );
 };
